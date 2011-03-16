07e927df32ba0f71a4e1d55051084cafb4e284ac
Christian Rech <rech.christian@googlemail.com>
2011-03-16 11:35:54 +0100

    marquise erstellt

--- src/de/htwhome/devices/Light.java
+++ src/de/htwhome/devices/Light.java
@@ -9,13 +9,4 @@ public class Light extends Actor<Boolean> {
     public Light(int id, boolean status, String location, String type, String hint, int[] gID) {
         super(id, status, location, type, hint, gID);
     }
-
-    public boolean getStatus() {
-        return (Boolean) this.status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
 }
