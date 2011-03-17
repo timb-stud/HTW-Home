51684ce508f27e8bb9a395af8b66245abee80545
vgoekkaya <volkan.goekkaya@googlemail.com>
2011-03-17 11:38:23 +0100

    Messagereceiver threaded statt nur die Methode receiver

--- src/de/htwhome/transmission/MessageReceiver.java
+++ src/de/htwhome/transmission/MessageReceiver.java
@@ -2,11 +2,13 @@ package de.htwhome.transmission;
 
 import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author Tobias Lana, Volkan Goekkaya
  */
-public class MessageReceiver {
+public class MessageReceiver extends Thread{
 
     static final int    PORT = 1234;
     private static final int BUFFERSIZE = 256;
@@ -15,21 +17,24 @@ public class MessageReceiver {
     
     public MessageReceiver() throws SocketException {
         sock = new DatagramSocket(PORT);
-        
     }
     
-    public synchronized void receiver() throws IOException {
+    @Override
+    public synchronized void run(){
         while (true) {
-            pack = new DatagramPacket(new byte[BUFFERSIZE], BUFFERSIZE);
-            sock.receive(pack);
-//            ServerThread st = new ServerThread(sock, pack);
-            ServerThread st = new ServerThread(pack);
-            st.start();
+            try {
+                pack = new DatagramPacket(new byte[BUFFERSIZE], BUFFERSIZE);
+                sock.receive(pack);
+                //            ServerThread st = new ServerThread(sock, pack);
+                ServerThread st = new ServerThread(pack);
+                st.start();
+            } catch (IOException ex) {
+                Logger.getLogger(MessageReceiver.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
     public static void main(String[] args) throws SocketException, IOException {
         MessageReceiver msgr = new MessageReceiver();
-        msgr.receiver();
     }
 }
