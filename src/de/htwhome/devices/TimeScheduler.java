d403f622af65e08eed2f51ac3cfeb4eb027be90e
Christian Rech <rech.christian@googlemail.com>
2011-03-21 10:13:22 +0100

    TimeScheduler

--- src/de/htwhome/devices/TimeScheduler.java
+++ src/de/htwhome/devices/TimeScheduler.java
@@ -5,44 +5,78 @@
 
 package de.htwhome.devices;
 
-import de.htwhome.Main;
+import com.google.gson.Gson;
+import de.htwhome.transmission.Message;
+import de.htwhome.transmission.MessageSender;
+import de.htwhome.transmission.MessageType;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author christian
  */
-public class TimeScheduler {
+public class TimeScheduler extends Sensor{
     private Timer timer;
 
-    public TimeScheduler(int[] gid, long from, long till) {
+    public TimeScheduler(long from, long till) {
         timer = new Timer();
         long start = from * 1000;  //TODO Berechnung
         long duration = till * 1000;
-        timer.schedule(new TimeSchedulerTask(999), start, duration);
+        timer.schedule(new TimeSchedulerTask<Boolean>(gid, true), start, duration);
     }
-    
-    class TimeSchedulerTask extends TimerTask{
-        private boolean status = false;
-        private int id;
 
-        public TimeSchedulerTask(int id) {
-            this.id = id;
-        }
+    @Override
+    public void handleMsg(String msg) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-        @Override
-        public void run() {
-            status = (status) ? false : true;
-            System.out.format("ID = " + id + " Status = " + status + "%n");
-//            timer.cancel(); //Terminate the timer thread
-        }
-        
+    @Override
+    public void setStatus(Object status) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
      public static void main(String args[]) {
-        int[] gid = {1,2};
-        new TimeScheduler(gid, 2, 5);
+        new TimeScheduler(2, 5);
         System.out.format("Task scheduled.%n");
     }
+}
+
+
+class TimeSchedulerTask<T> extends TimerTask{
+    private boolean changeStatus = false;
+    private int gid;
+    private T status;
+
+    public TimeSchedulerTask(int gid, T status) {
+        this.gid = gid;
+        this.status = status;
+    }
+
+    @Override
+    public void run() {
+        changeStatus = (changeStatus) ? false : true;
+        System.out.format("Status = " + status + " Statusswitcher = " + changeStatus + "%n");
+//            timer.cancel(); //Terminate the timer thread
+        Message<Boolean> msg = new Message<Boolean>();
+        msg.setMsgType(MessageType.statusChange);
+        msg.setReceiverId(this.gid);
+        msg.setStatus((Boolean) this.status);
+        this.sendMsg(msg, );
+    }
+
+    private void sendMsg(Message<T> msg, Type msgTyp){
+        try {
+            String json = new Gson().toJson(msg, msgTyp);
+//            System.out.println("JSON:" + json); //TODO aufraeumen
+            MessageSender.sendMsg(json);
+        } catch (IOException ex) {
+            Logger.getLogger(Actor.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 }
