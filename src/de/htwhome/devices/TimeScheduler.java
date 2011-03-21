862f6f6ae0540c1bf7ea86936fa7c61620b347dd
Christian Rech <rech.christian@googlemail.com>
2011-03-21 11:13:55 +0100

    TimeScheduler überarbeitet

--- src/de/htwhome/devices/TimeScheduler.java
+++ src/de/htwhome/devices/TimeScheduler.java
@@ -12,6 +12,7 @@ import de.htwhome.transmission.MessageSender;
 import de.htwhome.transmission.MessageType;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.SocketException;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
@@ -26,11 +27,23 @@ public class TimeScheduler<E> extends Sensor<Boolean>{
 
     private Timer timer;
 
-    public TimeScheduler(E status,long from, long till) {
+    public TimeScheduler() {
+        super.load();
+    }
+
+    public TimeScheduler(int id, boolean status, String location, String description, int[] actorIdTab, Boolean[] actorStatusTab, int gid) throws SocketException {
+        super(id, status, location, description, actorIdTab, actorStatusTab, gid);
+    }
+
+    public void startScheduler(E status,long from, long till){
         timer = new Timer();
         long start = from * 1000;  //TODO Berechnung
         long duration = till * 1000;
         timer.schedule(new TimeSchedulerTask<E>(gid, status), start, duration);
+    }
+
+    public void stopScheduler(){
+        timer.cancel(); //Terminate the timer thread
     }
 
     @Override
@@ -50,56 +63,56 @@ public class TimeScheduler<E> extends Sensor<Boolean>{
     }
 
 
-     public static void main(String args[]) {
-        new TimeScheduler<Boolean>(true,2, 5);
-        System.out.format("Task scheduled.%n");
-    }
-}
+     
+ //===============(Start interne Klasse)================>
+        class TimeSchedulerTask<T> extends TimerTask{
+            public final Type msgType;
+
+            private boolean changeStatus = false;
+            private int gid;
+            private T status;
 
 
-class TimeSchedulerTask<T> extends TimerTask{
-    public final Type msgType;
+            public TimeSchedulerTask(int gid, T status) {
+                this.gid = gid;
+                this.status = status;
 
-    private boolean changeStatus = false;
-    private int gid;
-    private T status;
+                if (this.status instanceof Boolean)
+                    msgType = new TypeToken<Message<Boolean>>(){}.getType();
+                else if (this.status instanceof Integer)
+                    msgType = new TypeToken<Message<Integer>>(){}.getType();
+                else if (this.status instanceof Double)
+                    msgType = new TypeToken<Message<Double>>(){}.getType();
+                else
+                    msgType = null; //TODO msgType muss initialisiert werden. null führt zu fehler
+            }
 
-    public TimeSchedulerTask(int gid, T status) {
-        this.gid = gid;
-        this.status = status;
+            @Override
+            public void run() {
+                changeStatus = (changeStatus) ? false : true;
+                System.out.format("Status = " + status + " Statusswitcher = " + changeStatus + "%n");
+        //            timer.cancel(); //Terminate the timer thread
+                Message<T> msg = new Message<T>();
+                msg.setMsgType(MessageType.statusChange);
+                msg.setReceiverId(this.gid);
+                msg.setStatus(this.status);
+                this.sendMsg(msg, this.msgType);
+            }
 
-        if (this.status instanceof Boolean)
-            msgType = new TypeToken<Message<Boolean>>(){}.getType();
-        else if (this.status instanceof Integer)
-            msgType = new TypeToken<Message<Integer>>(){}.getType();
-        else if (this.status instanceof Double)
-            msgType = new TypeToken<Message<Double>>(){}.getType();
-        else
-            msgType = null;
-    }
+            //TODO handleResponse
 
-    @Override
-    public void run() {
-        changeStatus = (changeStatus) ? false : true;
-        System.out.format("Status = " + status + " Statusswitcher = " + changeStatus + "%n");
-//            timer.cancel(); //Terminate the timer thread
-        Message<T> msg = new Message<T>();
-        msg.setMsgType(MessageType.statusChange);
-        msg.setReceiverId(this.gid);
-        msg.setStatus(this.status);
-        this.sendMsg(msg, this.msgType);
-    }
+            private void sendMsg(Message<T> msg, Type msgTyp){
+                try {
+                    String json = new Gson().toJson(msg, msgTyp);
+        //            System.out.println("JSON:" + json); //TODO aufraeumen
+                    MessageSender.sendMsg(json);
+                } catch (IOException ex) {
+                    Logger.getLogger(Actor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
 
-    //TODO handleResponse
-
-    private void sendMsg(Message<T> msg, Type msgTyp){
-        try {
-            String json = new Gson().toJson(msg, msgTyp);
-//            System.out.println("JSON:" + json); //TODO aufraeumen
-            MessageSender.sendMsg(json);
-        } catch (IOException ex) {
-            Logger.getLogger(Actor.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-
+//================(ende interne Klasse)========================>
 }
+
+
