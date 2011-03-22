f4afd191a5382defa19082301383b8c6f2859ed3
Tobias Lana <tobias.lana@googlemail.com>
2011-03-22 22:13:03 +0100

    SmokeDetector hat nun fireAlarm handling

--- src/de/htwhome/devices/SmokeDetector.java
+++ src/de/htwhome/devices/SmokeDetector.java
@@ -1,8 +1,11 @@
 package de.htwhome.devices;
 
 import com.google.gson.reflect.TypeToken;
+import de.htwhome.transmission.Message;
+import de.htwhome.transmission.MessageType;
 import de.htwhome.utils.SensorConfig;
 import java.lang.reflect.Type;
+import java.net.SocketException;
 
 /**
  *
@@ -13,9 +16,17 @@ public class SmokeDetector extends Sensor<Boolean>{
     public static final DeviceType deviceType = DeviceType.SmokeDetector;
     public static final Type cfgType = new TypeToken<SensorConfig<Boolean>>(){}.getType();
 
+    public SmokeDetector() {
+        super.load();
+    }
+    
+    public SmokeDetector (int id, Boolean status, String location, String description, int gid) throws SocketException {
+        super(id, status, location, description, gid);
+    }
+
     @Override
     public void setActorStatus(String status, int pos) {
-	throw new UnsupportedOperationException("Not supported yet.");
+	throw new UnsupportedOperationException("Not supported yet."); //TODO kann raus, oder?
     }
 
     @Override
@@ -33,8 +44,22 @@ public class SmokeDetector extends Sensor<Boolean>{
     public void setStatus(Boolean status) {
 	this.status = status;
 	if(this.status == true){
-	    
-	}
+            System.out.println("!! ACHTUNG !!");
+	    Message warning = new Message();
+            warning.setMsgType(MessageType.fireAlarm);
+            warning.setSenderId(this.id);
+            warning.setReceiverId(ALLDEVICES);
+            warning.setContent(String.valueOf(this.status));
+            warning.setSenderDevice(deviceType);
+            this.sendMsg(warning);
+        }
+	Message msg = new Message();
+        msg.setMsgType(MessageType.statusResponse);
+        msg.setSenderId(this.id);
+        msg.setReceiverId(this.gid);
+        msg.setContent(String.valueOf(this.status));
+	msg.setSenderDevice(deviceType);
+	this.sendMsg(msg);
     }
     
 }
