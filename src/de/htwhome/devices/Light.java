5af0b9828f107a02efe1da9741de357ffa0f233e
vgoekkaya <volkan.goekkaya@googlemail.com>
2011-03-22 15:04:15 +0100

    StatusChangedListener + in Light die erforderliche aenderung vorgenommen

--- src/de/htwhome/devices/Light.java
+++ src/de/htwhome/devices/Light.java
@@ -1,11 +1,14 @@
 package de.htwhome.devices;
 
 import com.google.gson.reflect.TypeToken;
+import de.htwhome.gui.StatusChangeEvent;
+import de.htwhome.gui.StatusChangeListener;
 import de.htwhome.transmission.Message;
 import de.htwhome.transmission.MessageType;
 import de.htwhome.utils.ActorConfig;
 import java.lang.reflect.Type;
 import java.net.SocketException;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  *
@@ -13,8 +16,10 @@ import java.net.SocketException;
  */
 public class Light extends Actor<Boolean> {
 
+    private final CopyOnWriteArrayList<StatusChangeListener> listeners = new CopyOnWriteArrayList<StatusChangeListener>();
     public static final DeviceType deviceType = DeviceType.Light;
-    public static final Type cfgType = new TypeToken<ActorConfig<Boolean>>(){}.getType();
+    public static final Type cfgType = new TypeToken<ActorConfig<Boolean>>() {
+    }.getType();
 
     public Light(int id, boolean status, String location, String description, int[] gidTab) throws SocketException {
         super(id, status, location, description, gidTab);
@@ -26,30 +31,46 @@ public class Light extends Actor<Boolean> {
 
     @Override
     public void setStatus(Boolean status) {
-	this.status = status;
-	Message msg = new Message();
-	msg.setMsgType(MessageType.statusResponse);
-	msg.setSenderId(this.id);
-	msg.setContent(String.valueOf(this.status));
-	msg.setSenderDevice(deviceType);
-	this.sendMsg(msg);
-        System.out.println("Light.status:" +  this.status);
+        this.status = status;
+        fireChangeEvent();
+        Message msg = new Message();
+        msg.setMsgType(MessageType.statusResponse);
+        msg.setSenderId(this.id);
+        msg.setContent(String.valueOf(this.status));
+        msg.setSenderDevice(deviceType);
+        this.sendMsg(msg);
+        System.out.println("Light.status:" + this.status);
     }
 
     @Override
     public void setStatus(String status) {
-	boolean b = Boolean.getBoolean(status);
-	this.setStatus(b);
+        boolean b = Boolean.getBoolean(status);
+        this.setStatus(b);
     }
 
     @Override
     public void handleMsg(String msg) {
-	super.handleMsg(msg, deviceType, cfgType);
+        super.handleMsg(msg, deviceType, cfgType);
+    }
+
+    public void addStatusChangeListener(StatusChangeListener l) {
+        this.listeners.add(l);
+    }
+
+    public void removeStatusChangeListener(StatusChangeListener l) {
+        this.listeners.remove(l);
+    }
+
+    protected void fireChangeEvent() {
+        StatusChangeEvent<Boolean> evt = new StatusChangeEvent<Boolean>(this, this.status);
+        for (StatusChangeListener l : listeners) {
+            l.changeEventReceived(evt);
+        }
     }
 
     public static void main(String[] args) throws SocketException {
-        int[] gid  = {1};
-	Light l = new Light(11, false, "haus", "Beschreibung", gid);
+        int[] gid = {1};
+        Light l = new Light(11, false, "haus", "Beschreibung", gid);
 //        Light l = new Light();
 //	l.handleMsg("{'gid': '1', 'status': 'false', 'action': 'changeStatus'}");
 
@@ -60,5 +81,4 @@ public class Light extends Actor<Boolean> {
 //                  + "\n" + "status= " + sc2.getStatus()
 //                );
     }
-
 }
