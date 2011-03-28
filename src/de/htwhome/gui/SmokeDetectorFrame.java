d6b08b638b7c51ee36a87e746bfcc636fe3d6c2e
vgoekkaya <volkan.goekkaya@googlemail.com>
2011-03-28 15:45:53 +0200

    Smokedetectorframe

---
+++ src/de/htwhome/gui/SmokeDetectorFrame.java
@@ -0,0 +1,95 @@
+package de.htwhome.gui;
+
+import de.htwhome.devices.SmokeDetector;
+import java.awt.Color;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Volkan Gökkaya
+ */
+public class SmokeDetectorFrame extends javax.swing.JFrame implements StatusChangeListener {
+
+    public SmokeDetector sd;
+
+    public SmokeDetectorFrame(int id, Boolean status, String location, String description) throws SocketException {
+        initComponents();
+        sd = new SmokeDetector(id, status, location, description);
+        sd.addStatusChangeListener(this);
+        this.setBackground(Color.green);
+        jLabel1.setText(" ");
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jToggleButton1 = new javax.swing.JToggleButton();
+        jLabel1 = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jToggleButton1.setText("SmokeDetector starten");
+        jToggleButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jToggleButton1MouseClicked(evt);
+            }
+        });
+
+        jLabel1.setText("jLabel1");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(67, 67, 67)
+                        .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(172, 172, 172)
+                        .addComponent(jLabel1)))
+                .addContainerGap(93, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jLabel1)
+                .addGap(32, 32, 32)
+                .addComponent(jToggleButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 192, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(62, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jToggleButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jToggleButton1MouseClicked
+        if (jToggleButton1.isSelected()) {
+            jLabel1.setText("Smoke Detector aktiviert....");
+        }
+    }//GEN-LAST:event_jToggleButton1MouseClicked
+
+    public void changeEventReceived(StatusChangeEvent evt) {
+        this.setBackground(Color.red);
+    }
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                try {
+                    new SmokeDetectorFrame(30, false, "Wohnzimmerdecke", "rauch hilfee").setVisible(true);
+                } catch (SocketException ex) {
+                    Logger.getLogger(SmokeDetectorFrame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JToggleButton jToggleButton1;
+    // End of variables declaration//GEN-END:variables
+}
