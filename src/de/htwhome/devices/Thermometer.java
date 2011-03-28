c9b6ad09e808ba11773337251b6bba028722e118
Tobias Lana <tobias.lana@googlemail.com>
2011-03-28 15:43:58 +0200

    Thermometer neu und Timer fuer Temp angepasst

---
+++ src/de/htwhome/devices/Thermometer.java
@@ -0,0 +1,65 @@
+package de.htwhome.devices;
+
+import com.google.gson.reflect.TypeToken;
+import de.htwhome.timer.TimeScheduler;
+import de.htwhome.timer.TimerOptions;
+import java.lang.reflect.Type;
+import java.net.SocketException;
+import de.htwhome.transmission.Message;
+import de.htwhome.transmission.MessageType;
+import de.htwhome.utils.Config;
+
+/**
+ *
+ * @author tobiaslana
+ */
+public class Thermometer extends IntervalSensor<Double>{
+
+    public static final DeviceType deviceType = DeviceType.Thermometer;
+    public static final Type cfgType = new TypeToken<Config<Double>>(){}.getType();
+
+    public Thermometer (int id) {
+	this.id = id;
+        super.loadConfig(deviceType);
+    }
+
+    public Thermometer (int id, Double status, String location, String description, int gid) throws SocketException {
+        super(id, status, location, description);
+    }
+
+    @Override
+    public void handleMsg(String msg) {
+        super.handleMsg(msg, deviceType, cfgType);
+    }
+
+    @Override
+    public void setStatus(Double status) {
+        this.status = status;
+	fireChangeEvent();
+	Message msg = new Message();
+        msg.setMsgType(MessageType.statusResponse);
+        msg.setSenderId(this.id);
+        msg.setContent(String.valueOf(this.status));
+	msg.setSenderDevice(deviceType);
+	this.sendMsg(msg);
+    }
+
+    @Override
+    public void setStatus(String status) {
+        double d = Double.valueOf(status);
+        this.setStatus(d);
+    }
+
+    private void startNotifier(int intervall){
+        TimeScheduler<Double> ts = new TimeScheduler<Double>(this, TimerOptions.THERMOMETER);
+        ts.startIntervallRandom(intervall);
+    }
+
+    public static void main(String[] args) throws SocketException {
+        Thermometer t = new Thermometer(11501, 0.0, "Garten", "Thermometer", ALLDEVICES);
+        t.startNotifier(5000);
+    }
+
+    @Override
+    protected void fireChangeEvent() {}
+}
