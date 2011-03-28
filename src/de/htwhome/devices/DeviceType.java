891a168621729e7bb4a6b3224972efab4e6a4ef6
Tim Bartsch <timb.stud@gmail.com>
2011-03-28 12:54:48 +0200

    rechtschreibfehler in devType behoben.

--- src/de/htwhome/devices/DeviceType.java
+++ src/de/htwhome/devices/DeviceType.java
@@ -6,5 +6,5 @@ package de.htwhome.devices;
  */
 public enum DeviceType {
     Light, Switch, Sunblind, PercentSwitch, Panel, Anemometer, SmokeDetector,
-    Shutter, Webcam, DoorOpene, OnOffTimer;
+    Shutter, Webcam, DoorOpener, OnOffTimer;
 }
